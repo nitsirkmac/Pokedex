90dec6ee18c84edc2b61580040cc2ce58c31c006
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-24 17:25:25 -0400

    Index route created and operational

---
+++ server.js
@@ -0,0 +1,61 @@
+// DEPENDENCIES
+const express = require('express');
+const app = express();
+const Pokemon = require('./models/pokemon.js');
+
+//Pulls environment variables into server.js from .env
+require('dotenv').config();
+const PORT = process.env.PORT
+
+// DATABASE CONNECTION
+
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+
+// I N D U C E S
+
+// INDEX
+app.get('/', (req, res) => {
+    // res.send("This route is stupid")
+    res.render('index.ejs', { 
+        data: Pokemon 
+    });
+})
+
+// NEW
+app.get("/pokemon/new", (req, res) => {
+    res.send("New Pokemon")
+})
+
+// DELETE
+app.delete("/pokemon/:id", (req, res) => {
+    res.send("Destroy")
+})
+
+// UPDATE
+app.put("/pokemon/:id", (req, res) => {
+    res.send("Update")
+})
+
+// CREATE
+app.post("/pokemon", (req, res) => {
+    res.send("Create")
+})
+
+// EDIT
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.send("Edit Pokemon")
+})
+
+// SHOW
+app.get('/:id', (req, res) => {
+    // res.send("Show Page")
+    res.render('show.ejs', {
+        data: Pokemon[req.params.id] })
+    })
+
+
+// LISTENER
+app.listen(PORT, (req, res) => {
+    console.log("Gotta catch 'em all!!!")
+})
